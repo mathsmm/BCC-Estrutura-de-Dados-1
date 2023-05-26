9300bc1f24d0bd1de894a7d8ab47393a0515ca78
Matheus Marchi Moro <71672437+mathsmm@users.noreply.github.com>
2023-05-26 18:18:36 -0300

    Alterações

--- busca/Vetor.cpp
+++ busca/Vetor.cpp
@@ -9,7 +9,15 @@ using namespace std;
 class Vetor
 {
 public:
-    // static vector<int> cria_ordenado(int n)
+    static vector<int> cria_ordenado(int n)
+    {
+        vector<int> v;
+        int i = 1;
+        while (i <= n)
+            v.push_back(i++);
+
+        return v;
+    }
 
     static vector<int> cria_embaralhado(int n)
     {
--- busca/main.cpp
+++ busca/main.cpp
@@ -15,7 +15,6 @@ int main()
     //     while (j <= 2)
     //     {
     //         vector<int> v = Vetor::cria_embaralhado((int)round(pow(10, j)));
-    //         // cout << Vetor::to_str(v) << endl;
     //         vv.push_back(v);
     //         j++;
     //     }
@@ -36,29 +35,35 @@ int main()
     vector<int> v3 = Vetor::cria_embaralhado((int)round(pow(10, 4)));
     vector<int> v4 = Vetor::cria_embaralhado((int)round(pow(10, 5)));
 
-    // vector<int> v5 = Vetor::cria_embaralhado((int)round(pow(10, 1)));
-    // vector<int> v6 = Vetor::cria_embaralhado((int)round(pow(10, 2)));
-    // vector<int> v7 = Vetor::cria_embaralhado((int)round(pow(10, 3)));
-    // vector<int> v8 = Vetor::cria_embaralhado((int)round(pow(10, 4)));
-    // vector<int> v9 = Vetor::cria_embaralhado((int)round(pow(10, 5)));
+    vector<int> v5 = Vetor::cria_ordenado((int)round(pow(10, 1)));
+    vector<int> v6 = Vetor::cria_ordenado((int)round(pow(10, 2)));
+    vector<int> v7 = Vetor::cria_ordenado((int)round(pow(10, 3)));
+    vector<int> v8 = Vetor::cria_ordenado((int)round(pow(10, 4)));
+    vector<int> v9 = Vetor::cria_ordenado((int)round(pow(10, 5)));
 
     cout << "Busca linear dos numeros 3, 30, 300, 3000 e 30000" << endl;
 
-    cout << to_string(Busca::busca_linear(v0, 3)) << endl;
-    cout << to_string(Busca::busca_linear(v1, 30)) << endl;
-    cout << to_string(Busca::busca_linear(v2, 300)) << endl;
-    cout << to_string(Busca::busca_linear(v3, 3000)) << endl;
-    cout << to_string(Busca::busca_linear(v4, 30000)) << endl;
-
-    // int i = 1;
-    // while (i <= 5)
-    //     cout << to_string(Busca::busca_linear(vv[i], 3 * (int)round(pow(10, i++ - 1)))) << endl;
+    cout << to_string(Busca::busca_linear(&v0, 3)) << endl;
+    cout << to_string(Busca::busca_linear(&v1, 30)) << endl;
+    cout << to_string(Busca::busca_linear(&v2, 300)) << endl;
+    cout << to_string(Busca::busca_linear(&v3, 3000)) << endl;
+    cout << to_string(Busca::busca_linear(&v4, 30000)) << endl;
 
     cout << "\nBusca binaria dos numeros 3, 30, 300, 3000 e 30000" << endl;
 
-    // int i = 6;
+    cout << to_string(Busca::busca_binaria(&v5, 3)) << endl;
+    cout << to_string(Busca::busca_binaria(&v6, 30)) << endl;
+    cout << to_string(Busca::busca_binaria(&v7, 300)) << endl;
+    cout << to_string(Busca::busca_binaria(&v8, 3000)) << endl;
+    cout << to_string(Busca::busca_binaria(&v9, 30000)) << endl;
+
+    // i = 1;
+    // while (i <= 5)
+    //     cout << to_string(Busca::busca_linear(&(vv[i]), 3 * (int)round(pow(10, i++ - 1)))) << endl;
+
+    // i = 6;
     // while (i <= 10)
-    //     cout << to_string(Busca::busca_linear(vv[i], 3 * (int)round(pow(10, i++ - 6)))) << endl;
+    //     cout << to_string(Busca::busca_binaria(&(vv[i]), 3 * (int)round(pow(10, i++ - 6)))) << endl;
 
     cout << "Fim" << endl;
 
